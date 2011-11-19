e9bde02b6eb9caaea242558e638c0a47aa048e60
Kulbir Saini <saini@saini.co.in>
2011-11-20 01:42:46 +0530

    Theme updates.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,9 @@
 module ApplicationHelper
   def image_for(value)
-    image_tag(value ? asset_path('icons/true.gif') : asset_path('icons/false.gif'))
+    image_tag(value ? asset_path('icons/true.png') : asset_path('icons/false.png'))
   end
 
-  def icon_for(action)
-    case action
-    when 'show'
-      image_tag(asset_path('icons/full.gif'))
-    when 'edit'
-      image_tag(asset_path('icons/edit.gif'))
-    when 'delete'
-      image_tag(asset_path('icons/delete.gif'))
-    end
+  def icon_for(action, title = '')
+    image_tag(asset_path("icons/#{action}.png"), :title => title)
   end
 end
