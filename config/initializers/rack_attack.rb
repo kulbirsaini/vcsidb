d4a5a4f136c1560e792afa1487805a8b41461478
Kulbir Saini <saini@saini.co.in>
2014-04-12 17:07:47 +0530

    Better throttle

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,11 +2,11 @@ limit = 5
 period = 5.minutes
 
 Rack::Attack.throttle("license_verification", limit: limit, period: period) do |req|
-  req.post? && req.ip && req.path == '/videoid'
+  "#{req.ip}#{req.params['id']}#{req.params['e']}#{req.params['un']}" if req.post? && req.path == '/videoid'
 end
 
 Rack::Attack.throttle("server_report", limit: limit, period: period) do |req|
-  req.post? && req.ip && req.path == '/servers'
+  "#{req.ip}#{req.params['server'].values.join()}" if req.post? && req.path == '/servers' && req.params['server'].present?
 end
 
 Rack::Attack.throttled_response = lambda do |env|
